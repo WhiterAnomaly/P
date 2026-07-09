d5e27569f8d229cbf94f48983d17b226cdbee383
WhiterAnomaly <kai906669@gmail.com>
2026-07-09 20:55:04 -0300

    Remove duplicate textBox initialization and start calls
    Removed duplicate initialization and start calls for textBox.

--- Core4/Script.js
+++ Core4/Script.js
@@ -1,3 +1,9 @@
 Core4.text.init("textBox");
 
 Core4.text.start("Esse texto aparece letra por letra.", 25);
+
+Core4.text.init("textBox");
+
+Core4.text.start("Esse texto aparece letra por letra.", 25);
+
+Core4.loop.start();
