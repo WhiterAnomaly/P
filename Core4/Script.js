a3690bf3f8e5b8408d26e1949cadd3344eea327c
WhiterAnomaly <kai906669@gmail.com>
2026-07-09 21:15:58 -0300

    Delete Core4 directory

--- Core4/Script.js
+++
@@ -1,9 +0,0 @@
-Core4.text.init("textBox");
-
-Core4.text.start("Esse texto aparece letra por letra.", 25);
-
-Core4.text.init("textBox");
-
-Core4.text.start("Esse texto aparece letra por letra.", 25);
-
-Core4.loop.start();
