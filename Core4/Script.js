3a9737de5e0fac4d6e3e35e99d9fc0f84c05b1a2
WhiterAnomaly <kai906669@gmail.com>
2026-07-09 21:34:37 -0300

    Add initial rendering and input setup

---
+++ Core4/Script.js
@@ -0,0 +1,11 @@
+// Inicializa o sistema de renderização
+Core4.render.init("gameCanvas");
+
+// Inicializa o sistema de entrada
+Core4.input.init();
+
+// Define o estado inicial da engine
+Core4.flow.change("boot");
+
+// Inicia o Game Loop
+Core4.loop.start();
