211a4913760809e8a79506495d414bf813fdccac
WhiterAnomaly <kai906669@gmail.com>
2026-07-09 22:02:55 -0300

    Initialize rendering and input systems

--- Core4/Script.js
+++ Core4/Script.js
@@ -9,3 +9,13 @@ Core4.flow.change("boot");
 
 // Inicia o Game Loop
 Core4.loop.start();
+
+// Inicializa os sistemas
+Core4.render.init("gameCanvas");
+Core4.input.init();
+
+// Estado inicial da engine
+Core4.flow.change("boot");
+
+// Inicia o Game Loop
+Core4.loop.start();
